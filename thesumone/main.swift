6a9962ed7ee14f1d30d062fa2711b250b57a1cc8
ciecmacs20171106578 <m15886077136@163.com>
2018-09-29 09:31:24 +0800

    Initial Commit

---
+++ thesumone/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  thesumone
+//
+//  Created by s20171106578 on 2018/9/29.
+//  Copyright © 2018年 s20171106578. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
